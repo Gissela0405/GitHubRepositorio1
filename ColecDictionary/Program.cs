afcca2dd4f5e2e107b68fcaf676b9afe868bb765
Gissela Maierena <gisselamairena95@gmail.com>
2023-06-09 22:00:06 -0600

    Mostrando diccionario sin ordenar y cambio de comentario

--- ColecDictionary/Program.cs
+++ ColecDictionary/Program.cs
@@ -3,7 +3,7 @@
 
 using System.Collections;
 String[] Departamento = { "Boaco", "Carazo", "Chinandega", "Chontales", "Costa Caribe Norte", "Costa Caribe Sur", "Estelí", "Granada", "Jinotega", "León", "Madriz", "Managua", "Masaya", "Matagalpa", "Nueva Segovia", "Río San Juan", "Rivas" };
-int[] Poblacion = { 185013, 197139, 439906, 190863, 530586, 414543, 229866, 214317, 475630, 421050, 174744, 1546939, 391903, 593503, 271581, 135446, 182645 };
+int[] Poblacion = { 185013, 197139, 439906, 190863, 530586, 414543, 229866, 214317, 475630, 421050, 174744,1575819,6939, 391903, 593503, 271581, 135446, 182645 };
 Dictionary<string, int> diccionario = Departamento
           .Zip(Poblacion, (k, v) => new { Clave = k, Valor = v })
           .ToDictionary(x => x.Clave, x => x.Valor);
@@ -19,7 +19,12 @@ for (var i = 0; i < Poblacion.Length; i++)
     if (Poblacion[i] == minDep)
         minDepkey = Departamento[i];
 }
-
+//mostrar el diccionario sin ordenar 
+Console.WriteLine($"datos Desordenados");
+foreach (var item in diccionario)
+Console.WriteLine($"{item.Key,-20}==>{item.Value,10:NO}");
+Console.WriteLine(); 
+//Ordenando con LINQ OrdeBy el diccionario 
 var ordenado = diccionario.OrderBy(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
 Departamento = ordenado.Keys.ToArray();
 Poblacion = ordenado.Values.ToArray();
